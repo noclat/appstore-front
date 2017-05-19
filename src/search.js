59866c3143198a910e1b0c1f7e9c53604e7545d6
noclat <noclat@gmail.com>
2017-05-19 12:03:19 +0200

    clear search

--- src/search.js
+++ src/search.js
@@ -3,11 +3,25 @@ import Component from 'inferno-component';
 class Search extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      value: '',
+    };
     this.handleKeyUp = this.handleKeyUp.bind(this);
+    this.clearField = this.clearField.bind(this);
   }
 
   handleKeyUp(e) {
+    this.setState({
+      value: e.target.value,
+    });
     this.props.query.setQuery(e.target.value).search();
+  }
+
+  clearField(e) {
+    this.setState({
+      value: '',
+    });
+    this.props.query.setQuery('').search()
   }
 
   render() {
@@ -20,7 +34,12 @@ class Search extends Component {
           rank <i class="material-icons">{ this.props.orderBy === 'rank:desc' ? 'arrow_drop_down' : 'arrow_drop_up' }</i>
         </button>
         <div className="Search-field">
-          <input className="Search-input" type="text" onKeyUp={ this.handleKeyUp } placeholder="Search the apps…" />
+          { this.state.value &&
+            <button className="Search-field-clear" type="button" onClick={ this.clearField }>
+              <i className="material-icons">clear</i>
+            </button>
+          }
+          <input className="Search-field-input" type="text" onKeyUp={ this.handleKeyUp } placeholder="Search the apps…" value={ this.state.value } />
         </div>
       </div>
     );
